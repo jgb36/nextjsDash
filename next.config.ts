0f75c4a2b6d0f286d4337bcbd912e267f22a2415
jgb36 <115026035+jgb36@users.noreply.github.com>
2025-04-09 11:01:04 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from 'next';
+import { ESLINT_DEFAULT_DIRS } from 'next/dist/lib/constants';
 
 const nextConfig: NextConfig = {
   experimental: {
     ppr: 'incremental'
-  }
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
+
 
 export default nextConfig;
